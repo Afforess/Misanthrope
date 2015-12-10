bdf9a12af4acc6a18724a7fb124206aa4a9fbc3a
Afforess <afforess@gmail.com>
2015-12-09 20:45:36 -0500

    Make time factor and pollution factor scale by number of ticks passed

--- libs/biter_expansion.lua
+++ libs/biter_expansion.lua
@@ -42,6 +42,7 @@ function BiterExpansion.new(logger)
         if expansion_phase.tick then
             expansion_phase:tick()
         end
+        self:update_expansion_factors(expansion_phase)
     end
 
     function self:update_expansion_phase()
@@ -86,34 +87,46 @@ function BiterExpansion.new(logger)
         global.last_expansion = game.tick
         state:update_expansion_state()
 
-        self:resetUnitGroup()
+        self:reset_unit_group()
 
         -- cause pollution to spread farther
         game.map_settings.pollution.diffusion_ratio = 0.04
         game.map_settings.pollution.min_to_diffuse = 50
+        
+        self.logger:log("Marathon mod enabled: " .. (self.is_marathon_enabled() and "true" or "false") .. ". RSO mod enabled: " .. (self.is_rso_enabled() and "true" or "false"))
+    end
 
-        -- penalize time more, and pollution a bit less (to compensate for the expanded spread)
-        -- penalize time and pollution more as the game time advances (10hrs, 24 hrs and 72 hrs respectively)
-        if game.tick < (10 * 60 * 60 * 60) then
-            game.map_settings.enemy_evolution.time_factor = 0.000004
-            game.map_settings.enemy_evolution.pollution_factor = 0.000015 / 4
-        elseif game.tick < (24 * 60 * 60 * 60) then
-            game.map_settings.enemy_evolution.time_factor = 0.000004 * 2
-            game.map_settings.enemy_evolution.pollution_factor = 0.000015 / 2
-            global.evo_factor = global.evo_factor * 1.5
-        elseif game.tick < (3 * 24 * 60 * 60 * 60) then
-            game.map_settings.enemy_evolution.time_factor = 0.000004 * 6
-            game.map_settings.enemy_evolution.pollution_factor = 0.000015
-            global.evo_factor = global.evo_factor * 2
+    function self:update_expansion_factors(state)
+        if state.evo_modifier > 0.99999 then
+            local ticks_played = game.tick
+            -- more generous to marathon or rso players
+            if self.is_marathon_enabled() then
+                ticks_played = ticks_played / 2
+            end
+            if self.is_rso_enabled() then
+                ticks_played = ticks_played / 2
+            end
+            -- At 12 hours, the time factor will be at 0.000004 (vanilla value).
+            -- after 108 hours of game play, max value of 0.00002 will be reached
+            game.map_settings.enemy_evolution.time_factor = math.min(0.00002, 0.000002 + 0.00000000000077160494 * game.tick)
+            -- after 64 hours of gameplay, max value of 0.000025 will be reached
+            game.map_settings.enemy_evolution.pollution_factor = math.max(0.000025, 0.000005 + 0.0000000000014467593 * game.tick)
         else
-            game.map_settings.enemy_evolution.time_factor = 0.000004 * 12
-            game.map_settings.enemy_evolution.pollution_factor = 0.000015 * 3
-            global.evo_factor = global.evo_factor * 3
+            game.map_settings.enemy_evolution.time_factor = 0
+            game.map_settings.enemy_evolution.pollution_factor = 0
         end
     end
 
+    function self.is_rso_enabled()
+        return remote.interfaces.RSO ~= nil
+    end
+
+    function self.is_marathon_enabled()
+        return game.item_prototypes["pipe"].stack_size == 100
+    end
+
     -- Defaults from base/prototypes/map-settings.lua
-    function self:resetUnitGroup()
+    function self:reset_unit_group()
     	game.map_settings.unit_group.min_group_gathering_time = 3600
     	game.map_settings.unit_group.max_group_gathering_time = 10 * 3600
 
